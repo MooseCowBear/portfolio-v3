1cf623684c353811ebc17d9967fe7e0f22efeab7
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-02-20 16:30:38 -0500

    add footer

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <div className="footer">
+      Made with{" "}
+      <span role="img" aria-label="love">
+        &hearts;
+      </span>{" "}
+      by me.
+    </div>
+  );
+}
