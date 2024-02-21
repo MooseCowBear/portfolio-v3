ac3e5df0b48a8ae331b5a71e959a13f7065d2623
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-02-21 11:39:35 -0500

    fill out error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,11 +1,25 @@
+import { useRouteError } from "react-router-dom";
+import { Navigation } from "../layouts/Navigation";
+import { Footer } from "../layouts/Footer";
+import "../styles/error.css";
+
 export default function Error() {
+  const error = useRouteError();
+  console.error(error);
+
   return (
-    <div>
-      <h1>Something isn't right...</h1>
-      <p>
-        Either we couldn't find what you were looking for, or we are
-        experiencing an unexpected difficulty. 
-      </p>
-    </div>
+    <>
+      <div className="wrapper">
+        <Navigation />
+        <div className="error">
+          <h1>Hmm, something is not quite right...</h1>
+          <p>
+            Either we couldn't find what you were looking for, or we are
+            experiencing an unexpected difficulty.
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
