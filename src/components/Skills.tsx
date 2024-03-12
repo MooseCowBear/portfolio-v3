db1f6ba6a4db59acf39404b818ab66d84aece123
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-12 13:29:27 -0400

    add skills

---
+++ src/components/Skills.tsx
@@ -0,0 +1,31 @@
+import Ruby from "../components/icons/Ruby.tsx"
+import Css from "./icons/Css.tsx";
+import Git from "./icons/Git.tsx";
+import Html from "./icons/Html.tsx";
+import JavaScript from "./icons/JavaScript.tsx"
+import Jest from "./icons/Jest.tsx";
+import Python from "./icons/Python.tsx"
+import RSpec from "./icons/RSpec.tsx";
+import Rails from "./icons/Rails.tsx"
+import React from "./icons/React.tsx";
+import Tailwind from "./icons/Tailwind.tsx";
+import TypeScript from "./icons/TypeScript.tsx";
+
+export default function Skills() {
+  return (
+    <div className="icons">
+      <Ruby />
+      <Python />
+      <JavaScript />
+      <TypeScript />
+      <Rails />
+      <React />
+      <Tailwind />
+      <Html />
+      <Css />
+      <RSpec />
+      <Jest />
+      <Git />
+    </div>
+  );
+}
